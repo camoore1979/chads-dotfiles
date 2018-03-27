8d4b9fe050067dee14a485499525386af3ee7d86
Chad Allen Moore <c.moore@vectren.com>
2018-03-27 10:59:57 -0500

    split aliases up into separate files into the ./aliases folder

---
+++ aliases/curl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create aliases for my curl commands
+alias curlG='curlGetTest'
+alias curlP='curlPostTest'
+alias curlPu='curlPutTest'
+alias curlD='curlDeleteTest'
+
+alias curlGH='curlGetTestwAuth'
+alias curlPH='curlPostTestwAuth'
+alias curlPuH='curlPutTestwAuth'
+alias curlDH='curlDeleteTestwAuth'
+
+alias getWAuth='curlGetWAuth'
+alias postWAuth='curlPostWAuth'
+alias putWAuth='curlPutWAuth'
+alias deleteWAuth='curlDeleteWAuth'
+
+alias loginChad='curlPostLogin'
---
+++ aliases/git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create aliases for git stuff
+alias gb='git branch'
+alias gs='git status'
+alias gbr='my_git_remote_branches_func'
+alias gc='my_git_commit_func'
+alias ga='my_git_add_func'
+alias gaa='git add -A'
+alias gl='my_long_git_stat_func'
+alias gg='my_git_graph'
+alias gitp='my_git_pretty'
+alias gg2='my_git_graph2'
