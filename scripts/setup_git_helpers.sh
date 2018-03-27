d6181042f28d116e08704471751ab814d7cab391
Chad Allen Moore <mr.chad.moore@gmail.com>
2018-03-27 12:38:23 -0400

    fixed bug in setup_git_helpers.sh

--- scripts/setup_git_helpers.sh
+++ scripts/setup_git_helpers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # add git info to command line prompt
-
-if [ ! "$(command -v brew)"]; then
+if [ -z "$(command -v brew)" ]; then 
 
   if [ -f ~/.bash-git-prompt/gitprompt.sh ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     source ~/.bash-git-prompt/gitprompt.sh
+    echo "bash-git-prompt loaded..."
   else
     echo "NOTE: you must have installed bash-git-prompt for this to work."
     echo "Brew is not available, use git to clone,"
@@ -19,6 +19,7 @@ else
       __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
       source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
       GIT_PROMPT_ONLY_IN_REPO=1
+    echo "bash-git-prompt loaded..."
   else
     echo "NOTE: you must have installed bash-git-prompt for this to work."
     echo "Use brew to install, e.g. $ brew install bash-git-prompt"
@@ -27,8 +28,7 @@ else
   
 fi
 
-
-if [ -f ~/dotfiles/git-completion.bash ]; then
+if [ -f ~/dotfiles/scripts/git-completion.bash ]; then
   . ~/dotfiles/scripts/git-completion.bash
   echo "git-completion.bash loaded"
 fi
