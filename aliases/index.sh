8c3457dfc5593b80443f92c2df02558d1586531d
Chad Allen Moore <mr.chad.moore@gmail.com>
2018-07-30 13:42:56 -0400

    added alias file for node / npm commands

--- aliases/index.sh
+++ aliases/index.sh
@@ -6,3 +6,4 @@ alias s='source ~/.bash_profile'
 . "$HOME/dotfiles/aliases/curl.sh"
 . "$HOME/dotfiles/aliases/git.sh"
 . "$HOME/dotfiles/aliases/linux_cl.sh"
+. "$HOME/dotfiles/aliases/node.sh"
---
+++ aliases/node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# create aliases for git stuff
+alias nr='npm run'
+alias nrt='npm test'
+alias nrl='npm run lint'
+alias nrs='npm start'
+
