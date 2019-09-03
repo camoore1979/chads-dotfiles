6152324638b26dac4d41cc9e5b2d58eb167e0153
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-09-03 11:02:20 -0400

    added a yarn start alias

--- aliases/node.sh
+++ aliases/node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# create aliases for git stuff
 alias nr='npm run'
 alias nrd='npm run develop'
 alias nrl='npm run lint'
 alias nrs='npm start'
 alias nrt='npm test'
+
+alias ys='yarn start'
 
