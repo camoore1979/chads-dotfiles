4a3c4fba498b3ad282d700973554ae70ca071cd8
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-10-01 10:48:36 -0400

    updated install script to automatically add sourcing of bash_aliases into the bash_profile

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-cp ./.bash_aliases  ~/.bash_aliases
+# create the .bash_aliases file from a template
+./create_bash_aliases.sh
 
-#this source is not working.. TODO: resolve
-source ~/.bash_aliases
+ACTUAL_PATH=$(pwd)
 
+if [ -f ~/.bash_profile ]
+  then
+    echo ".bash_profile exists!"
+    LINE_NUMBER=$(grep -n "/.bash_aliases" ~/.bash_profile | cut -d: -f 1);
+
+    if [ -n "$LINE_NUMBER" ] 
+      then
+      echo "sources .bash_aliases on line number $LINE_NUMBER, removing";
+      sed -i -e "$LINE_NUMBER d" ~/.bash_profile
+    else
+      echo ".bash_aliases not in .bash_profile"
+    fi 
+    echo "source $ACTUAL_PATH/.bash_aliases  # source aliases and scripts for chads dotfiles " >> ~/.bash_profile
+  else
+    echo ".bash_profile does NOT exist."
+fi
