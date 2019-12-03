e97ccc6838f628dadb52ba2557a7bd1d68c30268
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-12-03 14:43:10 -0500

    updated node and git commands for use of oh-my-zsh git plugin

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+# in order to not conflict with ohmyzsh git plugin
+# the following are prefixed with 'c'
+# for zsh git plugin see: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+
 # status
-alias gs='git status'
-alias gl='my_long_git_stat_func'
+alias cgs='git status'
+alias cgl='my_long_git_stat_func'
 
 # branch management
-alias gcd='git checkout development'
-alias gco='my_git_checkout_branch_func'
-alias gbr='my_git_remote_branches_func'
-alias gb='git branch'
+alias cgcd='git checkout development'
+alias cgco='my_git_checkout_branch_func'
+alias cgbr='my_git_remote_branches_func'
+alias cgb='git branch'
 
 # adding / committing
 alias gc='my_git_commit_func'
-alias ga='my_git_add_func'
-alias gaa='git add -A'
+alias cga='my_git_add_func'
+alias cgaa='git add -A'
 
 # pushing / pulling
-alias gp='git push'
-alias get='git pull'
-alias gpush='my_git_push_origin_func'
+alias cgp='git push'
+alias cget='git pull'
+alias cgpush='my_git_push_origin_func'
 
-# git log aliases
-alias gg='my_git_graph'
-alias gitp='my_git_pretty'
-alias gg2='my_git_graph2'
-
+# cgit log aliases
+alias cgg='my_git_graph'
+alias cgitp='my_git_pretty'
+alias cgg2='my_git_graph2'
--- aliases/node.sh
+++ aliases/node.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 alias nr='npm run'
+alias nrb='npm run build'
 alias nrd='npm run develop'
 alias nrl='npm run lint'
+alias nrlf='npm run lint-fix'
 alias nrs='npm start'
 alias nrt='npm test'
 
 alias ys='yarn start'
-
