3ec06c576d0dac5bd62f57012e6ce02e356ecf88
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-12-20 17:19:36 -0500

    added alias for some docker convenient commands - such as get id of last docker image built

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # docker images
-
 # cdls - list all docker images
 alias cdls='docker image ls'
 
 # cdb - docker build
 alias cdb='docker build .'
+
+# cdbn - docker build no cache
+alias cdbn='docker build . --no-cache'
+
 
 # cdbt - docker build, pass image name 
 my_docker_build_command() {
@@ -39,3 +42,33 @@ alias cdsa='docker stop $(docker ps -q)'
 
 # cdda - delete all running and stopped containers
 alias cdda='docker container rm -f $(docker ps -aq)'
+
+# cgdid - get latest docker image id
+alias cgdid='docker image ls -q | head -n 1 | pbcopy'
+
+# cgdid2 - get latest docker image id
+# my_docker_get_last_image_id() {
+#   local CGDID2_MY_DOCKER_IMAGE
+#   CGDID2_MY_DOCKER_IMAGE="$(docker image ls -q | head -n 1)"
+#   echo $CGDID2_MY_DOCKER_IMAGE_ID | pbcopy
+#   echo "$CGDID2_MY_DOCKER_IMAGE_ID copied to clipboard!"
+# }
+# alias cgdid2='my_docker_get_last_image_id'
+
+# my_test_function () {
+#   local func_result="some result 2"
+#   echo "$func_result"
+# }
+# alias myfunc='my_test_function'
+
+# cdrs - docker run and connect via shell command
+function my_docker_run_sh() {
+  docker run --entrypoint "/bin/sh" -it $1
+}
+alias cdrs='my_docker_run_sh'
+
+# cdcc - connect to running container 
+function my_docker_connect_to_container() {
+  docker exec -it $1 bash
+}
+alias cdcc='my_docker_connect_to_container'
