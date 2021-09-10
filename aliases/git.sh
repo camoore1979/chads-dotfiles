6359f33e8ba276fecfadc983344fe7bf060fc4da
Chad Allen Moore <mr.chad.moore@gmail.com>
2021-09-10 13:08:35 -0400

    cleaned up git aliases and funcs

--- aliases/git.sh
+++ aliases/git.sh
@@ -5,24 +5,10 @@
 # for zsh git plugin see: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
 
 # status
-alias cgs='git status'
 alias cgl='my_long_git_stat_func'
 
 # branch management
-alias cgcd='git checkout development'
-alias cgco='my_git_checkout_branch_func'
-alias cgbr='my_git_remote_branches_func'
-alias cgb='git branch'
-
-# adding / committing
-alias gc='my_git_commit_func'
-alias cga='my_git_add_func'
-alias cgaa='git add -A'
-
-# pushing / pulling
-alias cgp='git push'
-alias cget='git pull'
-alias cgpush='my_git_push_origin_func'
+alias cgbr='git branch -l | cat'
 
 # cgit log aliases
 alias cgg='my_git_graph'
