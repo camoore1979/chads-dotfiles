ddd3bf42a821349b7153936424025c5906ca5f5a
Chad Allen Moore <mr.chad.moore@gmail.com>
2023-02-24 11:30:58 -0500

    Added helper to create PR and view in the web.

--- aliases/index.sh
+++ aliases/index.sh
@@ -11,7 +11,6 @@ alias s='source_my_dotfiles'
 . "$PATH_TO_CHADS_DOTFILES/aliases/alias_helpers.sh"
 . "$PATH_TO_CHADS_DOTFILES/aliases/curl.sh"
 . "$PATH_TO_CHADS_DOTFILES/aliases/docker.sh"
-. "$PATH_TO_CHADS_DOTFILES/aliases/git.sh"
 . "$PATH_TO_CHADS_DOTFILES/aliases/linux_cl.sh"
 . "$PATH_TO_CHADS_DOTFILES/aliases/nav.sh"
 . "$PATH_TO_CHADS_DOTFILES/aliases/node.sh"
--- scripts/my_git_scripts.sh
+++ scripts/my_git_scripts.sh
@@ -9,21 +9,35 @@ my_git_fetch_and_checkout() {
     git checkout $1  
   fi
 }
+alias cgfco='my_git_fetch_and_checkout'
 
 my_long_git_stat_func() {
   git status > git.status.txt
   less git.status.txt
 }
+alias cgl='my_long_git_stat_func'
 
 my_git_graph() {
   git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
 }
+alias cgg='my_git_graph'
 
 my_git_graph2() {
   git log --date=short --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --abbrev-commit 
 }
+alias cgitp='my_git_pretty'
 
 my_git_pretty() {
   git log --pretty=format:"%h - %an, %ar : %s"
 }
+alias cgg2='my_git_graph2'
 
+function my_github_create_pr {
+  # use any options available for `gh pr create`
+  gh pr create $@
+
+  gh pr view -w
+}
+alias cghcp='my_github_create_pr'
+
+alias cgbr='git branch -l | cat'
